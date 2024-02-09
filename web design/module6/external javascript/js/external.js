351f12f6fa537c8ed0f4c425c6e7befcf004999d
“preranadave” <psdave.3399@gmail.com>
2024-02-09 16:05:31 +0530

    9/2 commit

---
+++ web design/module6/external javascript/js/external.js
@@ -0,0 +1,4 @@
+var a=20;
+var bb=30;
+var c=a+bb;
+console.log('additions of numbers is :',c)
---
+++ web design/module6/how to call javascript/add.js
@@ -0,0 +1,8 @@
+var a=10;
+var b=20;
+var ad=a+b;
+var subs=a-b;
+var mult=a*b;
+console.log("The additions of numbers is :",ad);
+console.log("The substractions of numbers is :",subs);
+console.log("The Multiplications of numbers is :",mult);
