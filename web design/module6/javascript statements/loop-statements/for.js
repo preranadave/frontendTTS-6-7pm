504864568ed309612b392c8ec24d79df328fb321
“preranadave” <psdave.3399@gmail.com>
2024-03-04 17:29:27 +0530

    javascript commit

---
+++ web design/module6/javascript statements/loop-statements/for.js
@@ -0,0 +1,32 @@
+// for : is a loop
+//      for loop are executed when condition is true 
+//         for loop used to print number of iteration repeted again and again there we applied for loop
+      
+//         syntax : for(initialization;condition;increment/decrement)
+//                 {
+//                      statements;
+//                 }
+
+
+for(var i=0;i<=5;i++)
+{
+    console.log(i);
+}
+
+//valid satemet
+ // var i=5;
+        // for(;i<=5;i++)
+        // {
+        //     console.log(i);
+        // }
+
+    //Printing name alphabet by alphabet
+    
+
+    // let text=""; //string
+    // const nm="Prerna";
+    // for(i=0;i<nm.length; i++)
+    // {
+    //     text=nm[i];
+    //     console.log(text);
+    // }
